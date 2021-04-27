940a9df764bf87b7d670fdfb27e6ae620a55c0ec
Tomas <tom.iaquinta@gmail.com>
2021-04-26 23:40:25 -0300

    progress on first notebook

---
+++ notebooks/utils.py
@@ -0,0 +1,9 @@
+"""
+Useful functions for the device operation, this file is based
+in the core code developed for the acoustic monitoring station
+and will be explained and used throughout the notebooks.
+
+In this file we will include:
+
+    - 
+"""
